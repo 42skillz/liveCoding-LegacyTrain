ec163a1733e8f056f1fae38149a5d9e0a4185238
Bruno Boucard <boucard.bruno@free.fr>
2018-04-09 17:47:57 +0200

    unsaved files

--- TrainTrain/WebTicketManager.cs
+++ TrainTrain/WebTicketManager.cs
@@ -7,47 +7,6 @@ using System.Threading.Tasks;
 
 namespace TrainTrain
 {
-    public interface IBookingReferenceService
-    {
-        Task<string> GetBookingReference();
-    }
-
-    public class BookingReferenceService : IBookingReferenceService
-    {
-        private readonly string _uriBookingReferenceService;
-
-        public BookingReferenceService(string uriBookingReferenceService)
-        {
-            _uriBookingReferenceService = uriBookingReferenceService;
-        }
-
-        public async Task<string> GetBookingReference()
-        {
-            string bookingRef;
-            using (var client = new HttpClient())
-            {
-                bookingRef = await GetBookRef(client);
-            }
-
-            return bookingRef;
-        }
-
-        public async Task<string> GetBookRef(HttpClient client)
-        {
-            var value = new MediaTypeWithQualityHeaderValue("application/json");
-            client.BaseAddress = new Uri(_uriBookingReferenceService);
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(value);
-
-            // HTTP GET
-            var response = await client.GetAsync("/booking_reference");
-            response.EnsureSuccessStatusCode();
-
-            var bookingRef = await response.Content.ReadAsStringAsync();
-            return bookingRef;
-        }
-    }
-
     public class WebTicketManager
     {
         private const string UriBookingReferenceService = "http://localhost:51691/";
